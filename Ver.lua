bac4abbac37762d512839e06a5c08e90e1ff51eb
danaton <razor-98@yandex.ru>
2025-06-20 00:02:14 +0400

    Initial commit, 2.0.1388 from October 2010

---
+++ Ver.lua
@@ -0,0 +1,5 @@
+assert(ZygorGuidesViewer,"Zygor Guides Viewer failed to load.")
+ZygorGuidesViewer.revision = tonumber(string.sub("$Revision: 1388 $", 12, -3))
+ZygorGuidesViewer.version = "2.0." .. ZygorGuidesViewer.revision
+ZygorGuidesViewer.date = string.sub("$Date: 2010-10-13 00:55:00 -0500 (Wed, 13 Oct 2010) $", 8, 17)
+--2010/09/21 08:30:37.37391
