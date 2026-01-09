73d760306f752060f2963d2f49ad5aef7c19e11c
danaton <razor-98@yandex.ru>
2026-01-09 20:50:43 +0400

    * Fixed an issue with flight points saving in the Russian client

--- Libs/LibTaxi-1.0/ruRU.lua
+++ Libs/LibTaxi-1.0/ruRU.lua
@@ -1,24 +1,69 @@
-if GetLocale()~="ruRU" then return end
+local locale = GetLocale()
+if locale ~= "ruRU" then
+    return
+end
 
-local addon,_L = ...
-assert(_L.Lib,"Library not loaded")
-if _L.Lib.TaxiNames_Local then return end
+local Lib = LibStub:GetLibrary("LibTaxi-1.0", true)
 
-_L.Lib.TaxiNames_Local = {
-	["Hellfire Peninsula, The Dark Portal"] = "Полуостров Адского Пламени, Темный портал",
+if not Lib then
+    print("|cffff0000LibTaxi: Библиотека не найдена!|r")
+    return
+end
 
-	-- Special
-	["Valley of Strength"] = nil, -- Orgrimmar
+--print("|cff00ff00LibTaxi: Инициализация русской локализации|r")
 
-	-- Special
-	["The Great Forge"] = nil, -- Ironforge
-	["Trade District"] = nil, -- Stormwind
+local existingLocal = Lib.TaxiNames_Local or {}
+local existingEnglish = Lib.TaxiNames_English or {}
 
-	-- Special
-	["Terrace of Light"] = nil, -- Shattrath City
-	["The Stair of Destiny"] = nil, -- The Dark Portal
-
-	-- Special
-	["Krasus' Landing"] = nil, -- Dalaran
-
+local russianTranslations = {
+	-- Альянс --
+	
+	
+	
+	-- Орда --
+	-- Калимдор --
+	["Moonglade"] = "Лунная поляна",
+	["Everlook"] = "Круговзор",
+	["Bloodvenom Post"] = "Застава Отравленной Крови",
+	["Emerald Sanctuary"] = "Изумрудное святилище", -- neutral
+	["Splintertree Post"] = "Застава Расщепленного Дерева",
+	
+	["Crossroads"] = "Перекресток",
+	["Thunder Bluff"] = "Громовой Утес",
+	["Ratchet"] = "Кабестан", -- neutral
+	["Camp Taurajo"] = "Лагерь Таурахо",
+	
+	
 }
+
+for en, ru in pairs(russianTranslations) do
+    existingLocal[en] = ru
+end
+
+Lib.TaxiNames_Local = existingLocal
+
+local englishFromRussian = {}
+for en, ru in pairs(existingLocal) do
+    englishFromRussian[ru] = en
+end
+
+Lib.TaxiNames_English = englishFromRussian
+
+Lib.minimap_exceptions = {
+    ["Торговый квартал"] = "Штормград",
+    ["Великая Кузня"] = "Стальгорн", 
+    ["Терраса Света"] = "Шаттрат",
+    ["Лестница Судьбы"] = "Полуостров Адского Пламени, Темный портал",
+    ["Аллея Силы"] = "Оргриммар"
+}
+
+local mt = { 
+    __index = function(t, key)
+        return key
+    end
+}
+
+setmetatable(Lib.TaxiNames_Local, mt)
+setmetatable(Lib.TaxiNames_English, mt)
+
+--print("|cff00ff00LibTaxi: Русская локализация успешно загружена|r")
